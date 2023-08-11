0ef856b3b2c06b6e3e2d37da06bd2bb06c5dd0db
Elsen <poozs@qq.com>
2023-08-11 17:28:03 +0800

    add strings api case go file

--- 1-basic/1-16-strings_and_strconv/1-strings.go
+++ 1-basic/1-16-strings_and_strconv/1-strings.go
@@ -15,5 +15,5 @@ var str = "Maybe let something else replace me in this world."
 func main() {
 	fmt.Println("判断字符串是否以 xx 开头: strings.HasPrefix(str, \"m\") =", strings.HasPrefix(str, "M"))
 	fmt.Println("判断字符串是否以 xx 结尾: strings.HasSuffix(str, \"d\") =", strings.HasSuffix(str, "d"))
-	fmt.Println("判断字符串是否包含 xx: strings.Contains(str, z) =", strings.Contains(str, "z"))
+	fmt.Println("判断字符串是否包含 xx 字符: strings.Contains(str, z) =", strings.Contains(str, "z"))
 }
