e3d838c90f324ca9633627cbdf450ee039769453
Elsen <poozs@qq.com>
2023-08-22 17:19:41 +0800

    basic: add 7-error.go file

--- 1-basic/1-23-function/7-error.go
+++ 1-basic/1-23-function/7-error.go
@@ -2,5 +2,16 @@
 7-error.go: 异常处理
 panic: 抛出异常
 recover: 捕获异常
+使用场景:
+
+	抛出一个panic异常, 然后再defer中通过recover捕获这个异常
+
+https://www.topgoer.com/%E5%87%BD%E6%95%B0/%E5%BC%82%E5%B8%B8%E5%A4%84%E7%90%86.html
 */
 package main
+
+var _sbc string
+
+func getSbc() string {
+	return _sbc
+}
--- 1-basic/1-23-function/main.go
+++ 1-basic/1-23-function/main.go
@@ -1,8 +1,14 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
 	// 闭包
 	closuresCase1()
 	closuresCase1()
+	fmt.Println(getSbc())
+
 }
